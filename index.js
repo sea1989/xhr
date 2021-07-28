4bb8d4ad585d1ad3c2eee924bc3690c54249daa8
Vic <52656327+sea1989@users.noreply.github.com>
2021-07-28 21:18:35 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -7,27 +7,23 @@ xhr.open("GET", "https://swapi.dev/api/people");
 // 3. Отсылаем запрос
 xhr.send();
 xhr.onload = function () {
-  if (xhr.status !== 200) {
-    // анализируем HTTP-статус ответа, если статус не 200, то произошла ошибка
-    alert(`Ошибка ${xhr.status}: ${xhr.statusText}`); // Например, 404: Not Found
-  } else {
-    // если всё прошло гладко, выводим результат
-    //alert(`Готово, получили ${xhr.response.length} байт`); // response -- это ответ сервера
+    if (xhr.status !== 200) {
+        // анализируем HTTP-статус ответа, если статус не 200, то произошла ошибка
+        alert(`Ошибка ${xhr.status}: ${xhr.statusText}`); // Например, 404: Not Found
+    } else {
+        // если всё прошло гладко, выводим результат
+        //alert(`Готово, получили ${xhr.response.length} байт`); // response -- это ответ сервера
 
-const response = xhr.response;
-const data = JSON.parse(response);
-console.log(data.results.length);
-//var my_div = newDiv = null;???
+        const response = xhr.response;
+        const data = JSON.parse(response);
+        console.log(data.results.length);
+        //var my_div = newDiv = null;???
 
-for (let i=0; i<data.results.length; i++){
+        for (let i = 0; i < data.results.length; i++) {
 
-addElement(data.results[i]);
-
-}
-
-
-
-  }
+            addElement(data.results[i]);
+        }
+    }
 };
 
 function addElement(data) {
@@ -36,12 +32,11 @@ function addElement(data) {
     // и добавляем в него немного контента
 
     var newDiv = document.createElement("tr");
-        newDiv.innerHTML = `<td>${data.name}</td><td>${data.mass}</td><td>${data.height}</td>`;
+    newDiv.innerHTML = `<td>${data.name}</td><td>${data.mass}</td><td>${data.height}</td>`;
 
     // Добавляем только что созданный элемент в дерево DOM
 
     my_div = document.getElementById("org_div1");
     //document.body.insertBefore(newDiv, my_div);
-  my_div.append(newDiv); 
-
+    my_div.append(newDiv);
 }
